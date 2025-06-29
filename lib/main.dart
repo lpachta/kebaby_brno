2a26f0adc4656ef6996046056a83a41b916c3013
lpachta <lukin.pachta@gmail.com>
2025-06-29 10:54:22 +0200

    User name reading now working

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:kebaby_brno/core/data/user_entry.dart';
 import 'package:kebaby_brno/core/widgets/app_scaffold_widget.dart';
@@ -20,6 +21,16 @@ class MyApp extends StatelessWidget {
   MyApp({super.key});
 
   final KebabDatabase db = KebabDatabase();
+
+  Future<DocumentSnapshot<Map<String, dynamic>>> _waitForUserData(
+    String uid,
+  ) async {
+    while (true) {
+      final doc = await db.getUserWithId(uid);
+      if (doc.exists && doc.data() != null) return doc;
+      await Future.delayed(const Duration(milliseconds: 300));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,20 +74,29 @@ class MyApp extends StatelessWidget {
 
             /// User is signed in
           } else {
-            return FutureBuilder(
-              future: db.getUserWithId(user.uid),
+            return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              future: _waitForUserData(user.uid),
               builder: (context, snapshot) {
-                if (snapshot.data == null) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
                   return const Scaffold(
                     body: Center(child: CircularProgressIndicator()),
                   );
                 }
-                final mapTemp = snapshot.data!.data();
-                if (mapTemp == null) {
-                  throw ("mapTemp should not be null because it should have been in the database what");
+
+                if (!snapshot.hasData ||
+                    snapshot.data == null ||
+                    snapshot.data!.data() == null) {
+                  return const Scaffold(
+                    body: Center(child: CircularProgressIndicator()),
+                  );
                 }
 
-                final UserEntry foundUser = UserEntry.fromJson(mapTemp);
+                final userData = snapshot.data!.data();
+                if (userData == null) {
+                  throw ("userData should not be null because it should have been in the database what");
+                }
+
+                final UserEntry foundUser = UserEntry.fromJson(userData);
 
                 return KebabAppScaffold(db: db, user: foundUser);
               },
